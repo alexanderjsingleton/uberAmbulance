de982044f858631ba48774b77714ecfe006681d1
alexanderjsingleton <alexanderjsingleton@gmail.com>
2019-05-30 16:09:54 -0500

    added mapbox tokens

--- db-operations.js
+++ db-operations.js
@@ -28,7 +28,7 @@ function fetchAmbulanceDetails(db, userId, callback) {
         userId: userId
     }, function(err, results) {
         if (err) {
-            console.log(err);
+            console.log(err)
         } else {
             callback({
                 ambulanceId: results.userId,
@@ -40,5 +40,61 @@ function fetchAmbulanceDetails(db, userId, callback) {
     });
 }
 
+function saveRequest(db, requestId, requestTime, location, citizenId, status, callback) {
+    db.collection("requestsData").insert({
+        "_id": requestId,
+        requestTime: requestTime,
+        location: location,
+        citizenId: citizenId,
+        status: status
+    }, function(err, results) {
+        if (err) {
+            console.log(err);
+        } else {
+            callback(results);
+        }
+    });
+}
+
+function updateRequest(db, issueId, ambulanceId, status, callback) {
+    db.collection("requestsData").update({
+        "_id": issueId
+    }, {
+        $set: {
+            status: status,
+            ambulanceId: ambulanceId
+        }
+    }, function(err, results) {
+        if (err) {
+            console.log(err);
+        } else {
+            callback("Issue updated")
+        }
+    });
+}
+
+function fetchRequests(db, callback) {
+    var collection = db.collection("requestsData");
+    //Using stream to process lots of records
+    var stream = collection.find({}, {
+        requestTime: 1,
+        status: 1,
+        location: 1,
+        _id: 0
+    }).stream();
+
+    var requestsData = [];
+
+    stream.on("data", function(request) {
+        requestsData.push(request);
+    });
+    stream.on('end', function() {
+        callback(requestsData);
+    });
+}
+
 exports.fetchNearestAmbulances = fetchNearestAmbulances;
 exports.fetchAmbulanceDetails = fetchAmbulanceDetails;
+exports.saveRequest = saveRequest;
+exports.updateRequest = updateRequest;
+exports.fetchRequests = fetchRequests;
